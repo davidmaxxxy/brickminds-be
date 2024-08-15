288ed5afaadf8e47a7d022ef06526e7db15ddf3c
Bernard Ayamboro <bernardayam493@gmail.com>
2024-08-15 22:25:24 +0100

    Mysql database connection from server side created

--- server.js
+++ server.js
@@ -1,12 +1,21 @@
 const express = require("express");
 const app = express();
-
+const connectToDatabase = require("./config/dbConnection");
 const PORT = 5000;
+const fs = require("fs");
 
 app.listen(PORT, (req, res) => {
   console.log(`server running ${PORT}`);
 });
 
 app.get("/", (req, res) => {
-  res.json({ message: "working" });
+  connectToDatabase.query("SELECT * FROM themes;", function (error, results) {
+    if (error) {
+      console.error("Error fetching users from the database: " + error.stack);
+      return res.status(500).json({ error: "Failed to fetch users" });
+    }
+
+    // Send the fetched data as a response
+    res.json(results);
+  });
 });
